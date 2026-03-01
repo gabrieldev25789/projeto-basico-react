ea19ea6fb79525c2ddfeb326753c8bc2d0e6f61a
Gabriel <gabrielsb1090@gmail.com>
2026-03-01 02:31:19 -0300

    Criando estrutura para mostrar albuns com base no artista que o user clicou

--- project-react-basic/src/assets/Components/Artists/Artists.jsx
+++ project-react-basic/src/assets/Components/Artists/Artists.jsx
@@ -1,33 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+// ARTISTAS 
 import kanye  from "../imgs/kanyewest.jpg"
 import mj from "../imgs/mj.webp"
 import drake from "../imgs/drake.jpg"
 
+// ALBUNS 
+import graduation from "../imgs/graduation.jpg"
+import bad from "../imgs/bad.jpg"
+import views from "../imgs/views.jpg"
+
 import "./Artists.css"
 
 function Artists() {
+   
+    function handleImg(nomeArtista) {
+  if (artistaSelecionado === nomeArtista) {
+    setArtistaSelecionado(null) // fecha se clicar de novo
+  } else {
+    setArtistaSelecionado(nomeArtista) // abre
+  }
+}
+     const [artistaSelecionado, setArtistaSelecionado] = useState(null)
+
   return (
     <>
     <div id="list-artists">
 
-    <div id='kanye' className='artist'>
-        <h2>KANYE WEST</h2>
-        <img src={kanye} alt="Kanye West" />  
-    </div>    
+        <div id='kanye' className='artist'>
+            <h2>KANYE WEST</h2>
+            <img onClick={() => handleImg("kanye")} src={kanye} alt="Kanye West" />  
+            {artistaSelecionado === "kanye" && 
+            <img src={graduation} className='album' alt="graduation-album" />}
+        </div>    
 
-    <div id='kanye' className='artist'>
-        <h2>Michael Jackson</h2>
-        <img src={mj} alt="Michael Jackson" /> 
- 
-    </div> 
+        <div id='kanye' className='artist'>
+            <h2>Michael Jackson</h2>
+            <img onClick={() => handleImg("Michael Jackson")} src={mj} alt="Michael Jackson" /> 
+            {artistaSelecionado === "Michael Jackson" &&  <img src={bad} className='album' alt="bad-album" /> }
 
-    <div id='kanye' className='artist'>
-        <h2>Drake</h2>
-        <img src={drake} alt="Drake" />  
-    </div>  
-</div>
+        </div> 
+
+        <div id='kanye' className='artist'>
+            <h2>Drake</h2>
+            <img onClick={()=> handleImg("Drake")} src={drake} alt="Drake" />  
+            {artistaSelecionado === "Drake" &&  <img src={views} className='album' alt="views-album" />}
+        </div>  
+
+    </div>
     </>
-
   )
 }
 
